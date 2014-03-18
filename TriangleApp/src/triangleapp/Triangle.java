bebf20244d8aeba630a93947d3234beb448143c9
dhayde5 <dhayde5@tigers.lsu.edu>
2014-03-18 14:43:33 -0500

    This is better. KCB needs to see.

--- TriangleApp/src/triangleapp/Triangle.java
+++ TriangleApp/src/triangleapp/Triangle.java
@@ -1,55 +1,243 @@
+/**
+ * @file Triangle.java
+ * @author Dustin Hayden
+ * @date 03-19-2014
+ * Description: 
+ * Course: CSC1350 Section 1
+ * pawsID: dhayde5
+ * Project #: 3
+ * Instructor: Dr. Duncan
+ */
+
 package triangleapp;
 import java.awt.geom.Point2D;
-/**
-* @file Triangle.java
-* @author Dustin Hayden
-* @date 03-19-2014
-* Description: This class models a triangle in a plane.
-* Course: CSC1350 Section 1
-* pawsID: dhayde5
-* Project #: 3
-* Instructor: Dr. Duncan
-*/
 
+/**
+ *
+ * @author dirty_000
+ */
 public class Triangle 
 {
-    /** 
-     * the first vertex of the triangle
+    /**
+     * The first vertex of the triangle.
      */
     private Point2D.Double vertexA;
     
-    /** 
-     * the second vertex of the triangle
+    /**
+     * The second vertex of the triangle.
      */
     private Point2D.Double vertexB;
     
-    /** 
-     * the third vertex of the triangle
+    /**
+     * The third vertex of the triangle.
      */
     private Point2D.Double vertexC;
     
-    /** 
-     * creates a triangle whose vertices are (0,0), (0,1) and (1,0)
+    /**
+     * Creates a triangle whose vertices are (0,0), (0,1) and (1,0).
      */
     public Triangle()
     {
-       vertexA = new Point2D.Double(0,0);
-       vertexB = new Point2D.Double(0,1);
-       vertexC = new Point2D.Double(1,0);
+       vertexA = new Point2D.Double(0, 0);
+       vertexB = new Point2D.Double(0, 1);
+       vertexC = new Point2D.Double(1, 0);
     }
     
-    /** 
-     * creates a triangle using the specified points
+     /**
+     * Creates a triangle using the specific points.
+     * @param pnt1 The first point.
+     * @param pnt2 The second point.
+     * @param pnt3 The third point.
+     * @throws IllegalArgumentException when the specified points are collinear.
      */
-    public Triangle(Point2D.Double point1, Point2D.Double point2, Point2D.Double point3)
+    public Triangle(Point2D.Double pnt1, Point2D.Double pnt2, Point2D.Double pnt3)
     {
-       vertexA = point1;
-       vertexB = point2;
-       vertexC = point3;  
+        if (pnt1.equals(pnt2) || pnt1.equals(pnt3) || pnt2.equals(pnt3))
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.x == pnt2.x && pnt2.x == pnt3.x)
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.y == pnt2.y && pnt2.y == pnt3.y)
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.x != pnt2.x && pnt2.x != pnt3.x)
+        {
+            if ((pnt1.y - pnt2.y) / (pnt1.x - pnt2.x) == (pnt2.y - pnt3.y) / (pnt2.x - pnt3.x))
+            {
+                throw new IllegalArgumentException("Two or more vertices are collinear.");
+            }
+        }
+        vertexA = pnt1;
+        vertexB = pnt2;
+        vertexC = pnt3;
     }
     
+    /**
+     * Changes the vertices of a triangle to the specified points.
+     * @param pnt1 The first new point.
+     * @param pnt2 The second new point.
+     * @param pnt3 The third new point.
+     * @throws IllegalArgumentException when the specified points are collinear.
+     */
+    public void setTriangle(Point2D.Double pnt1, Point2D.Double pnt2, Point2D.Double pnt3)
+    {
+        if (pnt1.equals(pnt2) || pnt1.equals(pnt3) || pnt2.equals(pnt3))
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.x == pnt2.x && pnt2.x == pnt3.x)
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.y == pnt2.y && pnt2.y == pnt3.y)
+        {
+            throw new IllegalArgumentException("Two or more vertices are collinear.");
+        }
+        else if (pnt1.x != pnt2.x && pnt2.x != pnt3.x)
+        {
+            if ((pnt1.y - pnt2.y) / (pnt1.x - pnt2.x) == (pnt2.y - pnt3.y) / (pnt2.x - pnt3.x))
+            {
+                throw new IllegalArgumentException("Two or more vertices are collinear.");
+            }
+        }
+        vertexA = pnt1;
+        vertexB = pnt2;
+        vertexC = pnt3;
+    }
+    
+    /**
+     * Gives the first vertex of this triangle.
+     * @return The first vertex of this triangle.
+     */
+    public Point2D.Double getVertexA()
+    {
+        return vertexA;
+    }
+    
+    /**
+     * Gives the second vertex of this triangle.
+     * @return The second vertex of this triangle.
+     */
+    public Point2D.Double getVertexB()
+    {
+        return vertexB;
+    }
+    
+    /**
+     * Gives the third vertex of this triangle.
+     * @return The third vertex of this triangle.
+     */
+    public Point2D.Double getVertexC()
+    {
+        return vertexC;
+    }
+    
+    /**
+     * Gives the length of the side opposite the first vertex.
+     * @return The length of the side opposite the first vertex.
+     */
     public double getLengthA()
     {
-        
+        return Math.sqrt(Math.pow((vertexA.x - vertexB.x), 2) + 
+                Math.pow((vertexA.y - vertexB.y), 2));
     }
+    
+    /**
+     * Gives the length of the side opposite the second vertex.
+     * @return The length of the side opposite the second vertex.
+     */
+    public double getLengthB()
+    {
+        return Math.sqrt(Math.pow((vertexB.x - vertexC.x), 2) + 
+                Math.pow((vertexB.y - vertexC.y), 2));
+    }
+    
+    /**
+     * Gives the length of the side opposite the third vertex.
+     * @return The length of the side opposite the third vertex.
+     */
+    public double getLengthC()
+    {
+        return Math.sqrt(Math.pow((vertexA.x - vertexC.x), 2) + 
+                Math.pow((vertexA.y - vertexC.y), 2));
+    }
+    
+     /**
+     * Gives a string representing this triangle in the format {(x1,y1),(x2,y2),(x3,y3)},
+     * where (x1,y1) are the coordinates of the first vertex, (x2,y2) are the coordinates
+     * of the second vertex, and (x3,y3) are the coordinates of the third vertex.
+     * 
+     * @return A string representing this triangle in the format {(x1,y1),(x2,y2),(x3,y3)},
+     * where (x1,y1) are the coordinates of the first vertex, (x2,y2) are the coordinates
+     * of the second vertex, and (x3,y3) are the coordinates of the third vertex.
+     */
+    @Override
+    public String toString()
+    {
+        return String.format("{(%.6f,%.6f),(%.6f,%.6f),(%.6f,%.6f)}", 
+            this.getVertexA().x, this.getVertexA().y, 
+            this.getVertexB().x, this.getVertexB().y,
+            this.getVertexC().x, this.getVertexC().y);
+    }
+    
+    /**
+     * Determines whether this triangle is an isosceles triangle.
+     * @return <code>true</code> if this triangle is an isosceles triangle; otherwise, </code>false</code>.
+     */
+    public boolean isIsosceles()
+    {
+        if (this.getLengthA() == this.getLengthB())
+        {
+            return true;
+        }
+        else if (this.getLengthB() == this.getLengthC())
+        {
+            return true;
+        }
+        else return this.getLengthB() == this.getLengthC();
+    }
+    
+     /**
+     * Determines whether this triangle is an equilateral triangle.
+     * @return <code>true</code> if this triangle is an equilateral triangle; otherwise, </code>false</code>.
+     */
+    public boolean isEquilateral()
+    {
+        return this.getLengthA() == this.getLengthB() && this.getLengthA() == this.getLengthC();
+    }
+    
+    /**
+     * Determines whether this triangle is a scalene triangle.
+     * @return <code>true</code> if this triangle is a scalene triangle; otherwise, </code>false</code>.
+     */
+    public boolean isScalene()
+    {
+        if (this.getLengthA() == this.getLengthB())
+        {
+            return false;
+        }
+        else return this.getLengthA() != this.getLengthC();
+    }
+    
+    /**
+     * Determines whether this triangle is a right triangle.
+     * @return <code>true</code> if this triangle is a right triangle; otherwise, </code>false</code>.
+     */
+    public boolean isRight()
+    {
+        if (Math.pow(this.getLengthA(), 2) == (Math.pow(this.getLengthB(), 2) + Math.pow(this.getLengthC(), 2)))
+        {
+            return true;
+        }
+        else if (Math.pow(this.getLengthB(), 2) == (Math.pow(this.getLengthA(), 2) + Math.pow(this.getLengthC(), 2)))
+        {
+            return true;
+        }
+        else return Math.pow(this.getLengthC(), 2) == (Math.pow(this.getLengthA(), 2) + Math.pow(this.getLengthB(), 2));
+    }
+    
 }
--- TriangleApp/src/triangleapp/TriangleTester.java
+++ TriangleApp/src/triangleapp/TriangleTester.java
@@ -1,16 +1,173 @@
+/**
+ * @file TriangleTester.java
+ * @author Dustin Hayden
+ * @date 03-19-2014
+ * Description: 
+ * Course: CSC1350 Section 1
+ * pawsID: dhayde5
+ * Project #: 3
+ * Instructor: Dr. Duncan
+ */
+
 package triangleapp;
+import java.awt.geom.Point2D;
+import java.util.Scanner;
 
 /**
- *
- * @author dirty_000
+ * Client program to test the Triangle class (see Triangle.java).
  */
-public class TriangleTester {
 
+public class TriangleTester 
+{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter the x- and y- coordinates of first vertex> ");
+        int vertA_x = s.nextInt();
+        int vertA_y = s.nextInt();
+        
+        System.out.print("Enter the x- and y- coordinates of second vertex> ");
+        int vertB_x = s.nextInt();
+        int vertB_y = s.nextInt();
+        
+        System.out.print("Enter the x- and y- coordinates of third vertex> ");
+        int vertC_x = s.nextInt();
+        int vertC_y = s.nextInt();
+        
+        Point2D.Double vertA = new Point2D.Double(vertA_x, vertA_y);
+        Point2D.Double vertB = new Point2D.Double(vertB_x, vertB_y);
+        Point2D.Double vertC = new Point2D.Double(vertC_x, vertC_y);
+      
+        Triangle triangle = new Triangle(vertA, vertB, vertC);
+        
+        System.out.println();
+        System.out.println("The triangle you just created is " + triangle + ".");
+        
+        double perimeter = triangle.getLengthA() + triangle.getLengthB() + triangle.getLengthC();
+        double halfPerimeter = perimeter / 2.0;
+        
+        System.out.printf("Perimeter = %.5f\n", perimeter);
+        
+        System.out.printf("Area      = %.5f\n", 
+                Math.sqrt(halfPerimeter * 
+                         (halfPerimeter - triangle.getLengthA()) * 
+                         (halfPerimeter - triangle.getLengthB()) * 
+                         (halfPerimeter - triangle.getLengthC())));
+        
+        System.out.println("Classification:");
+        
+        System.out.print("Right:       ");
+        if (triangle.isRight()) 
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Isosceles:   ");
+        if (triangle.isIsosceles())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Scalene:     ");
+        if (triangle.isScalene())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Equilateral: ");
+        if (triangle.isEquilateral())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Enter the new x- and y- coordinates of second vertex> ");
+        vertB_x = s.nextInt();
+        vertB_y = s.nextInt();
+        
+        // TODO: Why is this necessary? Why doesn't it just flow back to the first vertB when I reassign
+        // vertB_X and vertB_y?
+        Point2D.Double vertB2 = new Point2D.Double(vertB_x, vertB_y);
+       
+        Triangle triangle2 = new Triangle(vertA, vertB2, vertC);
+        
+        System.out.println();
+        System.out.println("After modifying the second vertex the triangle is " + triangle2 + ".");
+        
+        perimeter = triangle2.getLengthA() + triangle2.getLengthB() + triangle2.getLengthC();
+        
+        // TODO: Why is this necessary? Why doesn't it just flow through when I change perimeter?
+        halfPerimeter = perimeter / 2.0;
+        
+        System.out.printf("Perimeter = %.5f\n", perimeter);
+        
+        System.out.printf("Area      = %.5f\n", 
+                Math.sqrt(halfPerimeter * 
+                         (halfPerimeter - triangle2.getLengthA()) * 
+                         (halfPerimeter - triangle2.getLengthB()) * 
+                         (halfPerimeter - triangle2.getLengthC())));
+        
+        System.out.println("Classification:");
+        
+        System.out.print("Right:       ");
+        if (triangle2.isRight()) 
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Isosceles:   ");
+        if (triangle2.isIsosceles())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Scalene:     ");
+        if (triangle2.isScalene())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        
+        System.out.print("Equilateral: ");
+        if (triangle2.isEquilateral())
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
     }
-    
 }
+
+
